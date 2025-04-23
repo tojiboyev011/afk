dc38e3cb67beb470ac5c2511699725c046cfa038
tojiboyev011 <umidtojiboyev011@gmail.com>
2025-04-23 23:13:07 +0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,9 @@
+{
+  "dependencies": {
+    "minecraft-data": "^3.85.0",
+    "mineflayer": "^4.27.0",
+    "express": "^4.21.2",
+    "mineflayer-pathfinder": "^2.4.5",
+    "readline-sync": "^1.4.10"
+  }
+}
